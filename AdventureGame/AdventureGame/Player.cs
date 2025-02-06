1b203ae41491d607e21c87b0804d3d3bc80fb6ad
josephlegge <leggej@etsu.edu>
2025-02-06 15:28:37 -0500

    adventuregame

---
+++ AdventureGame/AdventureGame/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    internal class Player : ICharacter
+    {
+
+        private int health_;
+        public int Health { get; set; }
+
+
+
+        public string Heal(int healthRecovered)
+        {
+            if (healthRecovered + Health >= 100)
+            {
+                Console.WriteLine("Test");
+                Health = 100;
+                healthRecovered = 100 - healthRecovered;
+
+            }
+            else
+            {
+                Console.WriteLine("Test2");
+                Health += healthRecovered;
+            }
+            return $"Recovered {healthRecovered} health! You are now at {Health} HP!";
+        }
+
+        public string TakeDamage(int damageTaken)
+        {
+            if (Health < damageTaken)
+            {
+                Health = 0;
+            }
+            else
+            {
+                Health -= damageTaken;
+            }
+            return $"Took {damageTaken} damage! You are now at {Health} HP!";
+        }
+
+
+    }
+}
---
+++ AdventureGame/AdventureGame/Potion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    internal abstract class Potion
+    {
+
+        /// <summary>
+        /// The strength of the potion.
+        /// </summary>
+        public int PotionIntensity { get; set; }
+
+        public virtual string AffectPlayer(Player player)
+        {
+            return "E";
+        }
+
+
+    }
+}
